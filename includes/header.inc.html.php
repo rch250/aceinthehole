763ce0b537f8c19448e0b71ae5b8ffb670bca944
rch250 <50688367+rch250@users.noreply.github.com>
2019-06-11 21:09:03 -0700

    Add files via upload

---
+++ includes/header.inc.html.php
@@ -0,0 +1,6 @@
+<?php define('BASE_URL','/cas222/aceinthehole/'); ?>
+<header>
+    <h1>Ace in the Hole Multisport Events</h1>
+    <button class="hamburger">&#9776;</button>
+    <button class="cross">&#735;</button>
+</header>
